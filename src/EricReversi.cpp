fa6c3b9551c7be0d36bffbafa782b358ab4aeae2
ericxu233 <xuhanyang5@gmail.com>
2022-01-02 14:39:51 -0500

    finished basic game

--- src/EricReversi.cpp
+++ src/EricReversi.cpp
@@ -5,6 +5,7 @@ bool EricReversi::OnUserCreate()
     // Called once at the start, so create things here
     height = ScreenHeight();
     width = ScreenWidth();
+    board_dim_init();
     cout << height << " " << width << endl;
 	return true;
 }
@@ -13,11 +14,63 @@ bool EricReversi::OnUserUpdate(float fElapsedTime)
 {
     // called once per frame
 	Clear(olc::Pixel(101, 229, 227));
+    draw_board();
+    move();
     //usleep(10000);
+    for (int i = 0; i < pieceX.size(); i++) {
+        draw_piece(pieceX[i], pieceY[i], 2);
+    }
 	return true;
+}
+
+void EricReversi::board_dim_init() {
+    padding = 24;
+    bstartX = padding - 2;
+    bstartY = padding - 2;
+
+    bendX = width - padding*2 + bstartX;
+    bendY = width - padding*2 + bstartY;
+
+    stride = (width - padding*2)/8;
+    slen = stride - 1;
+
+    for (int i = 0; i < 8; i++) {
+        midX[i] = bstartX + i*stride + slen/2 + 1;
+        midY[i] = bstartY + i*stride + slen/2 + 1;
+    }
 }
 
 void EricReversi::draw_board() {
     olc::Pixel col(3, 82, 81);
-    
+
+    for (int i = 0; i < 9; i++) {
+        DrawLine(bstartX, bstartY + i*stride, bendX, bstartY + i*stride, col);
+        DrawLine(bstartX + i*stride, bstartY, bstartX + i*stride, bendY, col);
+    }
+}
+
+void EricReversi::move() {
+    auto click = GetMouse(0);
+    if (IsFocused() && click.bPressed) {
+        int mx = GetMouseX();
+        int my = GetMouseY();
+
+        if (mx < bstartX || mx > bendX || my < bstartY || my > bendY) return;
+
+        int px = (mx - bstartX)/stride;
+        int py = (my - bstartY)/stride;
+
+        add_piece(px, py);
+        //if ()
+    }
+}
+
+void EricReversi::draw_piece(int x, int y, int player) {
+    if(player == 1) FillCircle(midX[x], midY[y], slen*0.8, olc::WHITE);
+    else if(player == 2) FillCircle(midX[x], midY[y], slen*0.4, olc::BLACK);
+}
+
+void EricReversi::add_piece(int x, int y) {
+    pieceX.push_back(x);
+    pieceY.push_back(y);
 }
--- src/EricReversi.h
+++ src/EricReversi.h
@@ -8,9 +8,24 @@ public:
         sAppName = "Eric Reversi";
     }
 
-public:
+private:
     int height;
     int width;
+
+    //testing only
+    vector<int> pieceX;
+    vector<int> pieceY; 
+
+    //board specs
+    int padding;
+    int bstartX;
+    int bendX;
+    int bstartY;
+    int bendY;
+    int stride;
+    int slen;
+    int midX[8];
+    int midY[8];
 
 public:
     //core engine functions
@@ -19,6 +34,14 @@ public:
 
 private:
     //helper functions
+    void board_dim_init();
+    void move();
+
+    //game drawing routine
     void draw_board();
+    void draw_piece(int x, int y, int player);
+
+    //testing only
+    void add_piece(int x, int y);
 };
 
